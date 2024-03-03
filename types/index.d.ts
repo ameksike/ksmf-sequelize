23e0ebbbe6625068fef7ac6ca003a6a97f479077
amembrides <antonio.membrides@sita.aero>
2024-03-03 20:05:22 +0100

    update types

--- types/index.d.ts
+++ types/index.d.ts
@@ -11,6 +11,25 @@ declare const _exports: {
     load(dirname: string, callback?: Function): Manager;
     associate(): Manager;
     onLog(type: any, message: any): void;
+    logger: Console;
+    helper: any;
+    models: any;
+    option: {};
+    configure(payload?: any): import("ksmf/types/src/dao/DAOBase");
+    getUri(): string;
+    conn2str(cfg: string | {
+        dialect?: string;
+        username?: string;
+        password?: string;
+        database?: string;
+        protocol?: string;
+        host?: string;
+        port?: string;
+    }): string;
+    log(type: string | number, message: any): void;
+    onError(error: any): void;
+    onConnect(option: any): void;
+    onDisconnect(option: any): void;
 };
 export = _exports;
 import Manager = require("./src/DAOSequelize");
